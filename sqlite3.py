0f9b75edb3839663c901f78e2961e5595366812d
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-05-30 17:01:24 -0300

    Update sqlite3.py

--- sqlite3.py
+++ sqlite3.py
@@ -12,5 +12,10 @@ var_cursor = var_connection.cursor()
 var_cursor.execute(f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, weight REAL)')
 
 var_connection.commit()
+
+var_cursor.execute(f'INSERT INTO {TABLE_NAME} (id, name, weight) VALUES (NULL, "Carl Sagan", 10)')
+var_connection.commit()
+
+
 var_cursor.close()
 var_connection.close()
