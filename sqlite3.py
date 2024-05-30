81bf79986593be7c13197595bdbf420a33ca9b40
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-05-30 17:35:17 -0300

    Update sqlite3.py

--- sqlite3.py
+++ sqlite3.py
@@ -9,13 +9,15 @@ TABLE_NAME = 'customers'
 var_connection = sqlite3.connect(DATABASE_FILE)
 var_cursor = var_connection.cursor()
 
+var_cursor.execute(f'DELETE FROM {TABLE_NAME}')
+var_cursor.execute(f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"')
+var_connection.commit()
+
 var_cursor.execute(f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, weight REAL)')
-
 var_connection.commit()
 
-var_cursor.execute(f'INSERT INTO {TABLE_NAME} (id, name, weight) VALUES (NULL, "Carl Sagan", 10)')
+var_cursor.execute(f'INSERT INTO {TABLE_NAME} (id, name, weight) VALUES (NULL, "Carl Sagan", 10.5), (NULL, "Enéas Carneiro", 56)')
 var_connection.commit()
-
 
 var_cursor.close()
 var_connection.close()
