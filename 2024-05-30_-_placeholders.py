889ff51de930ff88fc198ad4b4c2183f010bb8ba
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-05-30 22:58:25 -0300

    Create 2024-05-30_-_placeholders.py

---
+++ 2024-05-30_-_placeholders.py
@@ -0,0 +1,24 @@
+import sqlite3
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+DATABASE_NAME = 'db.sqlite3'
+DATABASE_FILE = ROOT_DIR / DATABASE_NAME
+TABLE_NAME = 'customers'
+
+var_connection = sqlite3.connect(DATABASE_FILE)
+var_cursor = var_connection.cursor()
+
+var_cursor.execute(f'DELETE FROM {TABLE_NAME}')
+var_cursor.execute(f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"')
+var_connection.commit()
+
+var_cursor.execute(f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, weight REAL)')
+var_connection.commit()
+
+var_sql = f'INSERT INTO {TABLE_NAME} (name, weight) VALUES (?, ?)'
+var_cursor.execute(var_sql, ['Enéas', 100])
+var_connection.commit()
+
+var_cursor.close()
+var_connection.close()
