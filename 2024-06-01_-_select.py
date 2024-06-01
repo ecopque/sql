846781aa0d15e20ba9e20a454bda6d664e805277
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-06-01 11:38:03 -0300

    Update 2024-06-01_-_select.py

--- 2024-06-01_-_select.py
+++ 2024-06-01_-_select.py
@@ -5,6 +5,11 @@ var_connection = sqlite3.connect(DATABASE_FILE)
 var_cursor = var_connection.cursor()
 
 var_cursor.execute(f'SELECT * FROM {TABLE_NAME}')
+# var_cursor.execute(f'SELECT * FROM {TABLE_NAME} LIMIT 2') #1:
+# var_cursor.execute(f'SELECT * FROM {TABLE_NAME} WHERE id="3"') #3:
+
+# var_row = var_cursor.fetchone() #2:
+# print(var_row) #2:
 
 for in_row in var_cursor.fetchall():
     in_id, in_name, in_weight = in_row
