327381716e9c8cb2ca3905c47b8d784492ae36ee
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-06-03 15:31:34 -0300

    Create 2024-06-03_-_new_select.py

---
+++ 2024-06-03_-_new_select.py
@@ -0,0 +1,31 @@
+import sqlite3
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+DATABASE_NAME = 'db.sqlite3'
+DATABASE_FILE = ROOT_DIR / DATABASE_NAME
+TABLE_NAME = 'customers'
+
+var_connection = sqlite3.connect(DATABASE_FILE)
+var_cursor = var_connection.cursor()
+
+var_cursor.execute(f'DELETE FROM {TABLE_NAME}')
+var_cursor.execute(f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"')
+var_connection.commit()
+
+var_cursor.execute(f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, weight REAL)')
+var_connection.commit()
+
+var_sql = f'INSERT INTO {TABLE_NAME} (name, weight) VALUES (?, ?)'
+var_cursor.execute(var_sql, ('Lutz', 100))
+var_connection.commit()
+
+var_cursor.execute(f'SELECT * FROM {TABLE_NAME}')
+var_connection.commit()
+
+for in_row in var_cursor.fetchall():
+    in_in, in_name, in_weight = in_row
+    print(in_in, in_name, in_weight)
+
+var_cursor.close()
+var_connection.close()
