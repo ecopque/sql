03c6fe30b89c08d5dabfee65701cc87a756dbef0
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-06-06 21:22:51 -0300

    Update 2024-06-06_-_main.py

--- 2024-06-06_-_main.py
+++ 2024-06-06_-_main.py
@@ -6,7 +6,7 @@ import pymysql #1:
 import dotenv #2:
 import os #3:
 
-TABLE_NAME = 'customers'
+TABLE_NAME = 'customers' #14:
 dotenv.load_dotenv() #4:
 
 var_connection = pymysql.connect(host=os.environ['MYSQL_HOST'], user=os.environ['MYSQL_USER'], password=os.environ['MYSQL_PASSWORD'], database=os.environ['MYSQL_DATABASE'], charset='utf8mb4') #5:
@@ -17,11 +17,13 @@ print(os.environ['MYSQL_DATABASE']) #7: #8:
 with var_connection: #9:
     with var_connection.cursor() as cursor: #10:
         cursor.execute(f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(50) NOT NULL, age INT NOT NULL, PRIMARY KEY (id))') #13:
+        cursor.execute(f'TRUNCATE TABLE {TABLE_NAME}') #15:
         var_connection.commit()
         print(cursor)
     
-    with var_connection.cursor() as cursor:
-        var_result = cursor.execute(f'INSERT INTO {TABLE_NAME} (name, age) VALUES ("Edson", 100)')
+    with var_connection.cursor() as cursor: #16:
+        var_result = cursor.execute(f'INSERT INTO {TABLE_NAME} (name, age) VALUES ("Edson", 100)') #17:
+        var_resul2 = cursor.execute(f'INSERT INTO {TABLE_NAME} (name, age) VALUES ("Carl", 100), ("Enéas", 150)') #17:
         print(var_result)
         var_connection.commit()
 
