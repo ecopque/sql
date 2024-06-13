dd2cf88a0ba0c6e38dad7f2f39ef018ddad04aae
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-06-13 20:40:33 -0300

    Update 2024-06-10_-_cursor.py

--- 2024-06-10_-_cursor.py
+++ 2024-06-10_-_cursor.py
@@ -4,16 +4,16 @@ import dotenv
 import os
 
 TABLE_NAME = 'customers'
-dotenv.load_dotenv()
+dotenv.load_dotenv() #11:
 
-var_connection = pymysql.connect(host=os.environ['MYSQL_HOST'], user=os.environ['MYSQL_USER'], password=os.environ['MYSQL_PASSWORD'], database=os.environ['MYSQL_DATABASE'], charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+var_connection = pymysql.connect(host=os.environ['MYSQL_HOST'], user=os.environ['MYSQL_USER'], password=os.environ['MYSQL_PASSWORD'], database=os.environ['MYSQL_DATABASE'], charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor) #12:
 
 var_cursor = var_connection.cursor()
 
 with var_connection:
     with var_connection.cursor() as var_cursor:
         var_cursor.execute(f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(50) NOT NULL, age INT NOT NULL, PRIMARY KEY (id))')
-        var_cursor.execute(f'TRUNCATE TABLE {TABLE_NAME}')
+        var_cursor.execute(f'TRUNCATE TABLE {TABLE_NAME}') #13:
         var_connection.commit()
 
     with var_connection.cursor() as var_cursor:
@@ -28,8 +28,8 @@ with var_connection:
         var_sql_select = f'SELECT * FROM {TABLE_NAME}'
         var_cursor.execute(var_sql_select)
 
-        var_data = var_cursor.fetchall()
-        var_connection.commit()
+        var_data = var_cursor.fetchall() #15:
+        var_connection.commit() #14:
         
         print()
         for var_row in var_data:
@@ -37,17 +37,17 @@ with var_connection:
 
         print()
         for var_row in var_data:
-            print(var_row['name']) #2:
+            print(var_row['name']) #2: #16:
 
         print()
-        var_cursor.scroll(-2)
+        var_cursor.scroll(-2) #17:
         # var_cursor.scroll(1)
-        for var_row in var_cursor.fetchall():
+        for var_row in var_cursor.fetchall(): #18:
             print(var_row) #3:
         
         print()
-        var_cursor.scroll(2, 'absolute')
-        for var_row in var_cursor.fetchall():
+        var_cursor.scroll(2, 'absolute') #19:
+        for var_row in var_cursor.fetchall(): #20:
             print(var_row) #4:
 
         print()
@@ -61,9 +61,9 @@ with var_connection:
         var_data4 = var_cursor.fetchall()
         var_connection.commit()
         
-        print('len(var_data):', len(var_data)) #5:
-        print('len(var_data4):', len(var_data4)) #8:
-        print('var_cursor.rowcount:', var_cursor.rowcount) #6:
+        print('len(var_data):', len(var_data)) #5: #22:
+        print('len(var_data4):', len(var_data4)) #8: #23:
+        print('var_cursor.rowcount:', var_cursor.rowcount) #6: #21:
 
         print()
         print('rownumber', var_cursor.rownumber) #10:
@@ -72,7 +72,7 @@ with var_connection:
         var_data5 = ('Michigan', 300)
         var_cursor.execute(var_sql_insert, var_data5)
         var_connection.commit()
-        print('var_cursor.lastrowid:', var_cursor.lastrowid) #7:
+        print('var_cursor.lastrowid:', var_cursor.lastrowid) #7: #24:
 
         print()
         var_cursor.execute(f'SELECT id FROM {TABLE_NAME} ORDER BY id DESC LIMIT 1')
